4d3bbf6d755b8426f9a8ea5646d07859f831fd29
mhrapunou <mhrapunov@yandex.ru>
2020-09-25 21:57:41 +0300

    TrialFactory refactoring

--- src/main/java/by/epam/inner/factories/TrialFactory.java
+++ src/main/java/by/epam/inner/factories/TrialFactory.java
@@ -4,9 +4,13 @@ import by.epam.inner.beans.ExtraTrial;
 import by.epam.inner.beans.LightTrial;
 import by.epam.inner.beans.StrongTrial;
 import by.epam.inner.beans.Trial;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.log4j.Logger;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 import static by.epam.inner.constants.Constants.*;
 
@@ -14,58 +18,68 @@ public class TrialFactory {
     private static final Logger LOGGER = Logger.getLogger("logfile");
 
     public static Optional<Trial> getTrialFromFactory(JsonObject jsonObject) {
-        String className = jsonObject.get(CLASS_FIELD).getAsString();
-        JsonObject trialJSON = jsonObject.get(ARGS_FIELD).getAsJsonObject();
+        try {
+            Gson gson = new Gson();
+            String className = jsonObject.get(CLASS_FIELD).getAsString();
+            JsonObject trialJSON = jsonObject.get(ARGS_FIELD).getAsJsonObject();
 
-        if (!isNumberOfFieldsValid(jsonObject, trialJSON)) {
-            LOGGER.warn(EXTRA_DATA + ARRAY_DELIMITER + jsonObject.toString());
-            return Optional.empty();
-        }
+            Class<Trial> trialClass = (Class<Trial>) Class.forName("by.epam.inner.beans." + className);
 
-        String account = trialJSON.get(ACCOUNT_FIELD).getAsString();
-        int mark1 = trialJSON.get(MARK1_FIELD).getAsInt();
-        int mark2 = trialJSON.get(MARK2_FIELD).getAsInt();
+            if (!isNumberOfFieldsValid(jsonObject, trialJSON)) {
+                LOGGER.warn(EXTRA_DATA + ARRAY_DELIMITER + jsonObject.toString());
+                //return Optional.empty();
+            }
 
-        if (account.equals(EMPTY_STRING)) {
-            LOGGER.error(EMPTY_NAME + ARRAY_DELIMITER + jsonObject.toString());
-            return Optional.empty();
-        }
+            return Optional.ofNullable(gson.fromJson(trialJSON, trialClass))
+                    .filter(TrialFactory::isFieldsValid);
 
-        if (!isMarkValid(mark1)) {
-            LOGGER.error(WRONG_MARK1 + ARRAY_DELIMITER + jsonObject.toString());
-            return Optional.empty();
-        }
 
-        if (!isMarkValid(mark2)) {
-            LOGGER.error(WRONG_MARK2 + ARRAY_DELIMITER + jsonObject.toString());
-            return Optional.empty();
-        }
+           /*String account = trialJSON.get(ACCOUNT_FIELD).getAsString();
+            int mark1 = trialJSON.get(MARK1_FIELD).getAsInt();
+            int mark2 = trialJSON.get(MARK2_FIELD).getAsInt();
 
-        switch (className) {
-            case TRIAL:
-                return Optional.of(new Trial(account, mark1, mark2));
-            case LIGHT_TRIAL:
-                return  Optional.of(new LightTrial(account, mark1, mark2));
-            case STRONG_TRIAL:
-                return Optional.of(new StrongTrial(account, mark1, mark2));
-            case EXTRA_TRIAL:
-                int mark3 = trialJSON.get(MARK3_FIELD).getAsInt();
-                if (!isMarkValid(mark3)) {
-                    LOGGER.error(WRONG_MARK3 + ARRAY_DELIMITER + jsonObject.toString());
-                    return Optional.empty();
-                }
-
-                return Optional.of(new ExtraTrial(account, mark1, mark2, mark3));
-            default:
-                LOGGER.error(WRONG_CLASS_NAME + ARRAY_DELIMITER + jsonObject.toString());
+            if (account.equals(EMPTY_STRING)) {
+                LOGGER.error(EMPTY_NAME + ARRAY_DELIMITER + jsonObject.toString());
                 return Optional.empty();
+            }
 
+            if (!isMarkValid(mark1)) {
+                LOGGER.error(WRONG_MARK1 + ARRAY_DELIMITER + jsonObject.toString());
+                return Optional.empty();
+            }
+
+            if (!isMarkValid(mark2)) {
+                LOGGER.error(WRONG_MARK2 + ARRAY_DELIMITER + jsonObject.toString());
+                return Optional.empty();
+            }
+
+            switch (className) {
+                case TRIAL:
+                    return Optional.of(new Trial(account, mark1, mark2));
+                case LIGHT_TRIAL:
+                    return Optional.of(new LightTrial(account, mark1, mark2));
+                case STRONG_TRIAL:
+                    return Optional.of(new StrongTrial(account, mark1, mark2));
+                case EXTRA_TRIAL:
+                    int mark3 = trialJSON.get(MARK3_FIELD).getAsInt();
+                    if (!isMarkValid(mark3)) {
+                        LOGGER.error(WRONG_MARK3 + ARRAY_DELIMITER + jsonObject.toString());
+                        return Optional.empty();
+                    }
+
+                    return Optional.of(new ExtraTrial(account, mark1, mark2, mark3));
+                default:
+                    LOGGER.error(WRONG_CLASS_NAME + ARRAY_DELIMITER + jsonObject.toString());
+                    return Optional.empty();
+
+            }*/
+        }catch (ClassNotFoundException e){
+            LOGGER.error(WRONG_CLASS_NAME + ARRAY_DELIMITER + jsonObject.toString());
+            return Optional.empty();
         }
     }
 
-    private static boolean isMarkValid(int mark){
-        return mark >= 0 && mark <= 100;
-    }
+
 
     private static boolean isNumberOfFieldsValid(JsonObject jsonObject, JsonObject trialJson) {
         if (jsonObject.entrySet().size() == JSON_FIELDS_NUMBER) {
@@ -75,5 +89,16 @@ public class TrialFactory {
                 return trialJson.entrySet().size() == EXTRA_TRIAL_FIELDS_NUMBER;
             }
         }else return false;
+    }
+
+    private static boolean isFieldsValid(Trial trial){
+        return !trial.getAccount().isEmpty()
+                && isMarkValid(trial.getMark1())
+                && isMarkValid(trial.getMark2())
+                && (!(trial instanceof ExtraTrial) || isMarkValid(((ExtraTrial)trial).getMark3()));
+    }
+
+    private static boolean isMarkValid(int mark){
+        return mark >= 0 && mark <= 100;
     }
 }
