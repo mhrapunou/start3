df93694eafe77c5827c5f4be8bfce679dfc711c4
SergeySaloedov <sergeysaloedov@gmail.com>
2020-09-25 16:47:15 +0300

    TrialFactory & Constants upd

--- src/main/java/by/epam/inner/factories/TrialFactory.java
+++ src/main/java/by/epam/inner/factories/TrialFactory.java
@@ -13,6 +13,11 @@ public class TrialFactory {
     public static Optional<Trial> getTrialFromFactory(JsonObject jsonObject) {
         String className = jsonObject.get(CLASS_FIELD).getAsString();
         JsonObject trialJSON = jsonObject.get(ARGS_FIELD).getAsJsonObject();
+
+        if (!isNumberOfFieldsValid(jsonObject, trialJSON)) {
+            return Optional.empty();
+        }
+
         String account = trialJSON.get(ACCOUNT_FIELD).getAsString();
         int mark1 = trialJSON.get(MARK1_FIELD).getAsInt();
         int mark2 = trialJSON.get(MARK2_FIELD).getAsInt();
@@ -51,5 +56,15 @@ public class TrialFactory {
 
     private static boolean isMarkValid(int mark){
         return mark >= 0 && mark <= 100;
+    }
+
+    private static boolean isNumberOfFieldsValid(JsonObject jsonObject, JsonObject trialJson) {
+        if (jsonObject.entrySet().size() == JSON_FIELDS_NUMBER) {
+            if (!jsonObject.get(CLASS_FIELD).getAsString().equals(EXTRA_TRIAL)) {
+                return trialJson.entrySet().size() == TRIAL_FIELDS_NUMBER;
+            } else {
+                return trialJson.entrySet().size() == EXTRA_TRIAL_FIELDS_NUMBER;
+            }
+        }else return false;
     }
 }
