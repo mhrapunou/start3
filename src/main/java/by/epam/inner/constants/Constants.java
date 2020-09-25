c53ac81ceb166d54e728499f6e940f0a42f82b56
mhrapunou <mhrapunov@yandex.ru>
2020-09-25 08:58:57 +0300

    Introduction Beans classes & Constants

---
+++ src/main/java/by/epam/inner/constants/Constants.java
@@ -0,0 +1,10 @@
+package by.epam.inner.constants;
+
+public class Constants {
+	
+	public final static String MSG_PASSED = "\nThe number of passed trials = ";
+	public final static String MSG_SUM = "\nThe summs of the first and the second marks are";
+	public final static String ALL_FAILED = "\nwhether all trials are failed: ";
+	public final static String DELIMITER = ";";
+	
+}
